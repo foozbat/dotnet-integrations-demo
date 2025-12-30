9e6fc8620b0c19e890f295bb351cef574e01ff7c
foozbat <github@foozbat.net>
2025-12-29 21:19:52 -0600

    update hubspot record definition

--- HubspotContactRecord.cs
+++ HubspotContactRecord.cs
@@ -1,8 +1,6 @@
-using System.Text.Json.Serialization;
-
 namespace IntegrationsDemo;
 
 public record HubspotContactRecord(
-    [property: JsonPropertyName("external_contact_id")] string ExternalContactId,
-    [property: JsonPropertyName("hubspot_contact_id")] string HubspotContactId
+    string ExternalContactId,
+    string HubspotContactId
 );
